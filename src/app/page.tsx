41e4a222583e80212171836411941d225efaa2df
CHAMSSIDINE Abdallah Ambinintsoa <chamssidinezx347@gmail.com>
2025-10-03 08:22:00 +0000

    404 This page could not be found.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  // Redirect to a placeholder page as the dashboard is removed for now.
+  // Redirect to the professions page as the main dashboard.
   redirect('/professions');
 }
